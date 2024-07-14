e6a65bde429823956ec05f8aa201b6336706369d
iamFiz <gazisharinmostafiz@gmail.com>
2024-07-15 00:23:55 +0600

    Complete

--- Product.php
+++ Product.php
@@ -4,11 +4,28 @@ class Product{
    public $name;
    public $price;
 
-   public function __construct($id,$name,$price)
-   {
+   public function __construct($id,$name,$price){
     $this->id = $id;
     $this->name = $name;
     $this->price = $price;
    }
-   
+
+   function getFormattedPrice($price){
+    $this->price = sprintf('%.2f', $price);
+    return $this->price;
+   }
+
+   function showDetails(){
+    $heredoc = <<<EOD
+    Product Details:
+    - ID: {$this->id}
+    - Name: {$this->name}
+    - Price: \${$this->price}
+    EOD;
+    return $heredoc;
+   }
+
 }
+
+$product = new Product(1, 'T-shirt', 19.99);
+echo $product->showDetails();
